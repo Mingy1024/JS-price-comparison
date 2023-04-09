9c3923fc686aba9cf49a2b67d1c713fd958a5276
mingyo <mingyo2410@gmail.com>
2023-04-09 23:31:19 +0800

    完成 篩選資料 功能

--- all.js
+++ all.js
@@ -1,5 +1,6 @@
 const url = "https://hexschool.github.io/js-filter-data/data.json";
 const showList = document.querySelector(".showList");
+const buttonGroup = document.querySelector(".button-group");
 let data = [];
 
 // 串接 API & 顯示資料
@@ -7,12 +8,12 @@ function getData(){
     axios.get(url)
     .then((res)=>{
         data = res.data;
-        renderData();
+        renderData(data);
     })
 }
 getData();
 
-function renderData(){
+function renderData(data){
     let str = "";
     data.forEach((item)=>{
         str += `<tr>
@@ -27,3 +28,25 @@ function renderData(){
     })
     showList.innerHTML = str;
 }
+
+// 篩選資料
+buttonGroup.addEventListener("click",(e)=>{
+    if(e.target.type === "button"){
+       let type = e.target.dataset.type;
+       let filterData = [];
+       if(type === "N04"){
+        filterData = data.filter((item)=> item["種類代碼"] === "N04");
+       }else if(type === "N05"){
+        filterData = data.filter((item)=> item["種類代碼"] === "N05");
+       }else if(type === "N06"){
+        filterData = data.filter((item)=> item["種類代碼"] === "N06");
+       }
+
+       let sort = document.querySelectorAll(".button-group button");
+       sort.forEach((item)=>{
+        item.classList.remove("active");
+       })
+       e.target.classList.add("active");
+       renderData(filterData);
+    }
+})
